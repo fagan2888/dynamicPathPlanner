216e2feef5bdd26d2744f172b6a6bad6d00813ed
Pruthvi-Sanghavi <pruthvi@umd.edu>
2020-04-26 16:52:47 -0400

    commanding second controller

--- src/rtRrt.py
+++ src/rtRrt.py
@@ -5,13 +5,17 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 
 def move():
-    pub = rospy.Publisher('/dynamicPathPlanner/world_base_position_controller/command', Float64, queue_size=10)
+    pub1 = rospy.Publisher('/dynamicPathPlanner/world_base_1_position_controller/command', Float64, queue_size=10)
+    pub2 = rospy.Publisher('/dynamicPathPlanner/world_base_2_position_controller/command', Float64, queue_size=10)
     rospy.init_node('remote', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        data = 1
-        rospy.loginfo(data)
-        pub.publish(data)
+        data1 = 1
+        data2 = 5
+        rospy.loginfo(data1)
+        rospy.loginfo(data2)
+        pub1.publish(data1)
+        pub2.publish(data2)
         rate.sleep()
 
 if __name__ == '__main__':
